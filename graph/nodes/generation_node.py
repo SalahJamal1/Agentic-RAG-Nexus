2cb67a69a1ba350105b12cbacb011ce6a43d28ba
Salah <salahabufarha1996@outlook.com>
2026-09-09 07:18:15 +0300

    fix context

--- graph/nodes/generation_node.py
+++ graph/nodes/generation_node.py
@@ -5,7 +5,7 @@ from graph.state import GraphState
 def generation_node(state:GraphState)->GraphState:
     print("--Generation Node--")
     question=state["question"]
-    context=state["context"]
+    documents=state["documents"]
     retry_count=state.get("retry_count",0)+1
-    generation=generation_chain.invoke({"question":question,"context":context})
+    generation=generation_chain.invoke({"question":question,"context":documents})
     return  {"generation":generation,"retry_count":retry_count}
--- main.py
+++ main.py
@@ -16,5 +16,4 @@ async def main():
 
 
 if __name__ == "__main__":
-    # asyncio.run(main())
-    print(app.get_graph().draw_mermaid())
+    asyncio.run(main())
